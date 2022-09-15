7fd0bccdf1bacd90b86a82d179bfd345ecc7bfd5
Mahuna Msangi <abuuriwaida@gmail.com>
2022-09-14 18:31:23 -0700

    Task zero and 1

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <string.h>
+/**
+ * main - entry point
+ *
+ * Description: prints _putchar using putchar prototype
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char str[] = "_putchar";
+	int ch;
+
+	for (ch = 0; ch < 8; ++ch)
+	{
+		_putchar(str[]);
+	}
+	_putchar('\n');
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * prints_alphabet
+ */
+
+void print_alphabet(void)
+{
+	int herufi;
+
+	for (herufi = 'a'; herufi <= 'z'; ++herufi)
+	{
+		_putchar(herufi);
+	}
+	_putchar('\n');
+}
