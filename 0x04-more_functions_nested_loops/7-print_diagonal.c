3031a44af0c0c18183556eaf2b6a43ebac8524ed
Mahuna Msangi <abuuriwaida@gmail.com>
2022-09-15 19:07:21 -0700

    Feeling like diagonal in a parallel universe

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -20,15 +20,11 @@ void print_diagonal(int n)
 			for (j = 0; j < n; j++)
 			{
 				if (j == 1)
-				{
 					_putchar('\\');
-				}
 				else if (j < i)
-				{
 					_putchar(' ');
-				}
-				_putchar('\n');
 			}
+			_putchar('\n');
 		}
 	}
 }
