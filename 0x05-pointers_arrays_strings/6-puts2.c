980f09b66daaee78606a2135dba4bddfeb62566b
Mahuna Msangi <abuuriwaida@gmail.com>
2022-09-20 13:17:40 -0700

    Half the lies they tell about me aren't true

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * puts2 - prints every other character of a string, starting with the first character
+ * puts2 - prints every other character of a string
  * @str: input
  * Return: print
  */
