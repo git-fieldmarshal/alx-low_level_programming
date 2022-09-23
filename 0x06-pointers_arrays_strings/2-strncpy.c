1732041ab609284b6a7c19706a05d5915999f0b8
Mahuna Msangi <abuuriwaida@gmail.com>
2022-09-22 18:57:28 -0700

    Copying string

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -11,12 +11,12 @@ char *_strncpy(char *dest, char *src, int n)
 	int p;
 
 	p = 0;
-	if (j < n && src[p] != '\0')
+	while (j < n && src[p] != '\0')
 	{
 		dest[p] = src[p];
 		p++;
 	}
-	else if (p < n)
+	while (p < n)
 	{
 		dest[p] = '\0';
 		p++;
