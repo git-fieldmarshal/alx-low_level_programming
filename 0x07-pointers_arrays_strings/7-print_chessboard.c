be278abd228c6682f45af466a679f3a2bffcc044
Mahuna Msangi <abuuriwaida@gmail.com>
2022-09-27 07:25:10 -0700

    Chess is mental torture

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_chessboard - a function that prints chessboard
+ * @a: array
+ * @Return: Always 0
+ */
+void print_chessboard(char (*a)[8])
+{
+	int m;
+	int n;
+
+	for (m = 0; m < 8; m++)
+	{
+		for (n = 0; n < 8; n++)
+		{
+			putchar(a[m][n]);
+		}
+		putchar('\n');
+	}
+}
