84b2d9d1464a59562406b3393ba7031b7fb69c3c
Mahuna Msangi <abuuriwaida@gmail.com>
2022-09-27 07:15:15 -0700

    Task 5-strstr

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -8,5 +8,7 @@ char *_strchr(char *s, char c);
 char *_memset(char *s, char b, unsigned int n);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
+char *_strstr(char *haystack, char *needle);
 
 #endif /*MAIN_H*/
